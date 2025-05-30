e7e2a993c178883167041c1a058bde6b408ea6ea
daviliano2 <daviliano2@gmail.com>
2025-05-30 16:58:40 +0200

    Add cinamchine camera. Add rotation and boost controls to the rocket

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 10f;
+    [SerializeField] float rotationStrength = 10f;
+
+    Rigidbody rb;
+
+    void OnEnable()
     {
-        
+        thrust.Enable();
+        rotation.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+
+        if (rotationInput > 0)
+        {
+            // rb.freezeRotation = true;
+            // transform.Rotate(fixedRotation * Vector3.back);
+            // rb.freezeRotation = false;
+            ApplyRotation(Vector3.back);
+        }
+        else if (rotationInput < 0)
+        {
+            ApplyRotation(Vector3.forward);
+        }
+    }
+
+    private void ApplyRotation(Vector3 direction)
+    {
+        float fixedRotation = rotationStrength * Time.fixedDeltaTime;
+        rb.freezeRotation = true;
+        transform.Rotate(fixedRotation * direction);
+        rb.freezeRotation = false;
     }
 }
